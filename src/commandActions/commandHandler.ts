0f70983c208b1328c366531cccd30c08f255e077
Victor Bruno Paro <victorbparo@gmail.com>
2021-08-11 22:55:41 -0300

    Changing command handler from ifs to switch statement

--- src/commandActions/commandHandler.ts
+++ src/commandActions/commandHandler.ts
@@ -1,8 +1,17 @@
 const commandHandler = async (interaction: any): Promise<void> => {
-    if(interaction.commandName === 'test-response')
-        await interaction.reply(interaction.options.getString('your-text'));
-    else
-        console.log(interaction.commandName);
+    switch(interaction.commandName){
+            case 'test-response':
+                await interaction.reply(interaction.options.getString('your-text'));
+                //console.log('test response worked');
+                break;
+            default:
+                await interaction.reply('An error has ocurred, please try again. If the error persists, try to contact the developer about it.');
+                //console.log('default');
+    }
+    //if(interaction.commandName === 'test-response')
+    //    await interaction.reply(interaction.options.getString('your-text'));
+    //else
+    //    console.log(interaction.commandName);
 }
 
 export{
